0efa10589fb65374998d42e759e9ef980fc5a835
Emmancodex <megamindcodex@gmail.com>
2024-05-21 02:19:14 +0100

    fixed new message notification issues

--- src/controllers/changeHasRead.js
+++ src/controllers/changeHasRead.js
@@ -50,55 +50,55 @@ const change_hasRead_to_true = async (userId, receiverName, condition) => {
   }
 };
 
-const change_hasRead_to_false = async (userId, receiverName, condition) => {
-  try {
-    // console.log(`condition is ${condition}`);
-    const user = await User.findOne({ _id: userId });
-    const receiver = await User.findOne({ userName: receiverName });
+// const change_hasRead_to_false = async (userId, receiverName, condition) => {
+//   try {
+//     // console.log(`condition is ${condition}`);
+//     const user = await User.findOne({ _id: userId });
+//     const receiver = await User.findOne({ userName: receiverName });
 
-    if (!user) {
-      throw new Error("User not found");
-    }
+//     if (!user) {
+//       throw new Error("User not found");
+//     }
 
-    if (!receiver) {
-      throw new Error("Receiver not found");
-    }
+//     if (!receiver) {
+//       throw new Error("Receiver not found");
+//     }
 
-    const userName = user.userName;
-    // console.log(user);
-    const chatRooms = receiver.chatRooms;
+//     const userName = user.userName;
+//     // console.log(user);
+//     const chatRooms = receiver.chatRooms;
 
-    // console.log(chatRooms);
-    // console.log(receiverName, userName);
+//     // console.log(chatRooms);
+//     // console.log(receiverName, userName);
 
-    const chatMessage = chatRooms.find(
-      (chatRoom) =>
-        chatRoom.users.includes(userName) &&
-        chatRoom.users.includes(receiver.userName)
-    );
+//     const chatMessage = chatRooms.find(
+//       (chatRoom) =>
+//         chatRoom.users.includes(userName) &&
+//         chatRoom.users.includes(receiver.userName)
+//     );
 
-    if (!chatMessage) {
-      console.log("Couldn't find the specific chat message");
-    }
+//     if (!chatMessage) {
+//       console.log("Couldn't find the specific chat message");
+//     }
 
-    // Update the hasRead property of the chat room object within the chatRooms array
+//     // Update the hasRead property of the chat room object within the chatRooms array
 
-    // if (condition === undefined) {
-    // }
-    chatMessage.hasRead = condition;
+//     // if (condition === undefined) {
+//     // }
+//     chatMessage.hasRead = condition;
 
-    await receiver.save();
-    console.log(chatMessage);
+//     await receiver.save();
+//     console.log(chatMessage);
 
-    console.log(`${receiver.userName} hasRead is : ${chatMessage.hasRead}`);
-    console.log("hasRead updated successfully");
-    // console.log(user.chatRooms);
-    return chatMessage.hasRead;
+//     console.log(`${receiver.userName} hasRead is : ${chatMessage.hasRead}`);
+//     console.log("hasRead updated successfully");
+//     // console.log(user.chatRooms);
+//     return chatMessage.hasRead;
 
-    // Since chatRooms is an array of objects within the User document, you need to save the entire User document
-  } catch (err) {
-    console.log("Error toggling has read", err, err.messgae);
-  }
-};
+//     // Since chatRooms is an array of objects within the User document, you need to save the entire User document
+//   } catch (err) {
+//     console.log("Error toggling has read", err, err.messgae);
+//   }
+// };
 
-module.exports = { change_hasRead_to_true, change_hasRead_to_false };
+module.exports = { change_hasRead_to_true };
--- src/routes/changeHasReadRoute.js
+++ src/routes/changeHasReadRoute.js
@@ -1,32 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const { authorize } = require("../middleware/auth");
-const {
-  change_hasRead_to_true,
-  change_hasRead_to_false,
-} = require("../controllers/changeHasRead");
+const { change_hasRead_to_true } = require("../controllers/changeHasRead");
 
 router.put("/toggle_hasRead", authorize, async (req, res) => {
   try {
     const { receiverName, condition } = req.body;
     const userId = req.userId;
-    // console.log(receiverName, userId, condition);
+    console.log(
+      `the request body is: ${receiverName} condition is: ${condition}`
+    );
 
     if (condition === true) {
-      console.log(condition);
-      console.log(`Setting ${receiverName} hasRead to ${condition}`);
+      console.log(`Setting user hasRead to ${condition}`);
       const hasRead = await change_hasRead_to_true(
-        userId,
-        receiverName,
-        condition
-      );
-      res.status(200).json({ hasRead: hasRead });
-    }
-
-    if (condition === false) {
-      console.log(condition);
-      console.log(`Setting ${receiverName} hasRead to ${condition}`);
-      const hasRead = await change_hasRead_to_false(
         userId,
         receiverName,
         condition
